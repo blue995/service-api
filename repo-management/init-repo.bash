a9ae474aa6b6a23bafba28b8b30d8dc48bd07531
Tobias Blaufuß <t.blaufuss@gmx.de>
2020-01-28 01:31:03 +0100

    [UPDATE] init-repo script

--- repo-management/init-repo.bash
+++ repo-management/init-repo.bash
@@ -4,8 +4,9 @@ CURRENT_DIR=$PWD
 BASEDIR=$(dirname "$0")
 
 cd "$BASEDIR"/..
-git remote add main https://github.com/reportportal/service-api.git
-git fetch main
-git checkout --track main/master
+git remote rename origin patched
+git remote add origin https://github.com/reportportal/service-api.git
+git fetch origin
+git checkout --track origin/master
 git checkout service-api-with-tfs
 cd "$CURRENT_DIR"
