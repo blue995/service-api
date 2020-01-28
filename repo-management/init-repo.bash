4232d9201b72d77ce4c8a2f0b5633380efa7c527
Tobias Blaufuß <tobias.blaufuss@siemens-healthineers.com>
2020-01-28 15:20:00 +0100

    [UPDATE] Repo script without changing branches

--- repo-management/init-repo.bash
+++ repo-management/init-repo.bash
@@ -7,8 +7,8 @@ cd "$BASEDIR"/..
 git remote rename origin patched
 git remote add origin https://github.com/reportportal/service-api.git
 git fetch origin
-git checkout develop
-git branch --set-upstream-to origin/develop
-git checkout --track origin/master
+git branch develop --set-upstream-to origin/develop
+git branch master
+git branch master --set-upstream-to origin/master
 git checkout service-api-with-tfs
 cd "$CURRENT_DIR"
